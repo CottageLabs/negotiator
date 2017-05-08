437077ffded5b7b2a032be00168548bc3842910f
Kyle Fuller <kyle@fuller.li>
2017-05-08 19:58:10 +0100

    Add support for Python 3

--- negotiator/__init__.py
+++ negotiator/__init__.py
@@ -1,1 +1,1 @@
-from negotiator import AcceptParameters, ContentType, Language, ContentNegotiator
+from .negotiator import AcceptParameters, ContentType, Language, ContentNegotiator
--- setup.py
+++ setup.py
@@ -24,6 +24,10 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
 )
